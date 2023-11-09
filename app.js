7f44de79880302e7a7300d3cb9d4bb005be3cccd
Isaac Thomas <Isaac.Alexander.Thomas@outlook.com>
2023-11-09 14:34:29 -0600

    JsVariable

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
+let lastName = "Thomas";
+var age = 29;
 
 // Exercise 2
+const firstName= "Isaac"
 
 // Exercise 3
+
+let language = "javaScript";
+let createdYear = 1995;
+let inCaseSensitive = true;
+
+let price = 1995;
+let isOnsale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
